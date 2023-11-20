0f7e43c6eaeb8d36f4c0ca9af6224f9b602cb394
Ivan Carlini <ivancarlini99@gmail.com>
2023-11-20 22:36:45 +0100

    caricato modello Google

--- application/neural_style_transfer.py
+++ application/neural_style_transfer.py
@@ -6,8 +6,6 @@ from image_handler import tensor_to_image, load_image_from_file
 from model import NeuralNetwork
 
 ### PARAMETRI ###
-#content_url = 'https://media.gettyimages.com/id/1207663571/it/foto/empty-pavement-with-modern-architecture.jpg?s=612x612&w=gi&k=20&c=zs96UNIqroSsjZLtkS0VUww6lBUh1rZ3zq3QU0qaXAA='
-#style_url = 'https://p.turbosquid.com/ts-thumb/5m/fo2FbO/Kq/render/png/1679499783/600x600/fit_q87/d2223da563cc146e6647eb07a3396663a27647f8/render.jpg'
 output_file_name = 'result_image.png'
 
 #default (original content is more visible)
@@ -36,7 +34,7 @@ total_variation_weight = 30
 
 def transfer(content_image_file: datastructures.file_storage.FileStorage,
              style_image_file: datastructures.file_storage.FileStorage,
-             epochs, steps_per_epoch, mode):
+             epochs: int, steps_per_epoch: int, mode: int):
     
     content_image_file_data = content_image_file.read()
     style_image_file_data = style_image_file.read()
@@ -63,8 +61,9 @@ def transfer(content_image_file: datastructures.file_storage.FileStorage,
 
         print("Total time: {:.1f}".format(end-start))
     else:
-        hub_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
-        image = hub_model(tf.constant(content_image), tf.constant(style_image))[0]
+        #hub_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+        model = tf.keras.models.load_model("./model")
+        image = model(tf.constant(content_image), tf.constant(style_image))[0]
 
     tensor_to_image(image).save('./static/' + output_file_name)
     return
